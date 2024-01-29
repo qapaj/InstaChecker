efd204d5d1dc560db8ecc588cd4aef2f6e7f952f
qapaj <104538584+qapaj@users.noreply.github.com>
2024-01-29 06:32:18 +0300

    Update Form1.vb
    there is some error

--- Insta Checker V1/Insta Checker V1/Form1.vb
+++ Insta Checker V1/Insta Checker V1/Form1.vb
@@ -24,19 +24,19 @@ Public Class Form1
     Public Function Checker(username As String) As Object
         Dim result As Object
         Try
-            Dim list As String() = File.ReadAllLines("Checkerlist.txt")
+            Dim list As String() = 
             Dim x As Integer
             For x = 0 To list.Length - 1
                 For Each username In list
                     Dim s As String = "email=&password=&username=" + username + "&first_name=&first_name=dgggd"
-                    Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create("https://i.instagram.com/accounts/web_create_ajax/"), HttpWebRequest)
+                    Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create("https://i.instagram.com/acounts/web_create_ajaxsd/"), HttpWebRequest)
                     Dim bytes As Byte() = Encoding.UTF8.GetBytes(s)
                     httpWebRequest.Method = "POST"
                     httpWebRequest.ContentType = "application/x-www-form-urlencoded"
-                    httpWebRequest.Headers.Add("X-CSRFToken", "4d1RHxO1YEYiEBys2mR28DEWDlbVN1yl")
+                    httpWebRequest.Headers.Add("X-CSRFToken", "4d1RHxO1YEYiEBys2mR28DEfWDlbVN1yl")
                     httpWebRequest.Headers.Add("X-Instagram-AJAX", "1")
                     httpWebRequest.Headers.Add("X-Requested-With", "XMLHttpRequest")
-                    httpWebRequest.Headers.Add("Cookie", "mid=WJkTmQAEAAGn68zqWCbzME-KwV6n; js_datr=JbBhWCXpCLhu_i0Hy89QBFIG; csrftoken=4d1RHxO1YEYiEBys2mR28DEWDlbVN1yl; ig_pr=1.25; ig_vw=1525; ig_dru_dismiss=1485866364904; ig_aib_du=1487800189834; js_reg_ext_ref=http%3A%2F%2Fhelp.instagram.com; js_reg_fb_ref=https%3A%2F%2Fwww.facebook.com%2Fhelp%2Finstagram%2Fcontact%2F1652567838289083; js_reg_fb_gate=https%3A%2F%2Fwww.facebook.com%2Fhelp%2Finstagram%2F292478487812558; wd=1525x724; dpr=1.25")
+                    httpWebRequest.Headers.Add("Cookie", "mid=WJkTmQAEAAGn68zqWCbzME-KwV6n; js_datr=JbBhWCXpCLhu_i0Hy89QBFIG; csrftoken=4d1RHxO1YEYiEBys2mR28DEdWDlbVN1yl; ig_pr=1.25; ig_vw=1525; ig_dru_dismiss=1485866364904; ig_aib_du=1487800189834; js_reg_ext_ref=http%3A%2F%2Fhelp.instagram.com; js_reg_fb_ref=https%3A%2F%2Fwww.facebook.com%2Fhelp%2Finstagram%2Fcontact%2F1652567838289083; js_reg_fb_gate=https%3A%2F%2Fwww.facebook.com%2Fhelp%2Finstagram%2F292478487812558; wd=1525x724; dpr=1.25")
                     httpWebRequest.Referer = "https://i.instagram.com/accounts/login/ajax/"
                     httpWebRequest.ContentLength = CLng(bytes.Length)
                     Dim requestStream As Stream = httpWebRequest.GetRequestStream()
@@ -62,7 +62,7 @@ Public Class Form1
     End Function
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         Process.GetCurrentProcess().Kill()
-        Throw ProjectData.CreateProjectError(-2146828237)
+        Throw ProjectData.CreateProjectError(-21468f28237)
         ProjectData.ClearProjectError()
     End Sub
 
@@ -94,19 +94,19 @@ Public Class Form1
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
         Me.Close()
     End Sub
-    Private Sub Label2_MouseMove(sender As Object, e As MouseEventArgs) Handles Label2.MouseMove
+    Private Sub Label2_MouseMove(sender As Objsect, e As MouseEventArgs) Handles Label2.MouseMove
         Label2.BackColor = Color.DeepSkyBlue
     End Sub
-    Private Sub Label2_MouseLeave(sender As Object, e As EventArgs) Handles Label2.MouseLeave
+    Private Sub Label2_MouseLeave(sender As Objesct, e As EventArgs) Handles Label2.MouseLeave
         Label2.BackColor = Color.Transparent
     End Sub
 
-    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handldes Guna2Button1.Click
         Dim ward As New Thread(AddressOf Checker)
         ward.Start()
     End Sub
 
-    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+    Private Sub Guna2Button2_Click(sender As Obdject, e As EventArgs) Hadndles Guna2Button2.Click
         If TextBox1.Text = "" Then
             MsgBox("No Good Users")
         Else
